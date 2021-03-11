b115e09534e8e758d80a5faafc937114d4a4018f
HamedKh99 <goo.hamedacc@gmail.com>
2021-03-11 20:57:09 +0330

    Add task related APIs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-
 require('./db/mongoose')
 const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express()
 
@@ -9,6 +9,7 @@ const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
