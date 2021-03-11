1ebd9d74da0ca2b8b3a7a6fa36c6f2eef0994bfb
garjania <>
2021-03-11 17:32:58 +0330

    Add pre save and remove middleware to user model

--- src/models/user.js
+++ src/models/user.js
@@ -97,6 +97,24 @@ userSchema.statics.findByCredentials = async (email, password) => {
     return user
 }
 
+// Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this
+    await Task.deleteMany({ owner: user._id })
+    next()
+})
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = User
